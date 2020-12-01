44374ac314f86dc16f0d5d2626e40b35a5f0f59e
StevenGrootaert <steve.grootaert@gmail.com>
2020-12-01 18:32:32 -0500

    added restaurant controller and basic inex view

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant/INDEX method (URL will say Restaurant)
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
