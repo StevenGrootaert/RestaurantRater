f5bdb06809724f2d40b6374798c4379b2f5784a2
StevenGrootaert <steve.grootaert@gmail.com>
2020-12-01 22:41:33 -0500

    added the edit functions

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -64,6 +65,41 @@ namespace RestaurantRater.Controllers
             _db.Restaurants.Remove(restaurant); // remove the restuarant in the database
             _db.SaveChanges(); // save the changes (_field is just a snapshot of the current Database)
             return RedirectToAction("Index"); // return to the index page after deletion
+        }
+
+        // GET: Restuarant/Edit/{id}
+        // get an id from the user
+        // find a rest by that id
+        // handle if the id in null
+        // if the rest doesn't exist
+        // return the rest and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id); // Restuarant Object called restuarnt set equal to a snapshot/calling our database(db called restuarants)then call that find method pass in restuarnt by an {id}
+            if (restaurant == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound); // why not just say return HttpNotFound ??
+            }
+            return View();
+        }
+
+
+        // POST: Restuarant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restuarant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restuarant).State = EntityState.Modified; // Access the state property find entry min 27:30 in video ... apply only modied rows in database
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restuarant);
         }
     }
 }
